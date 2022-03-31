a080cbdb87bf0aa9b9c3d2605e203d66a6caf7f8
Come1LLF00 <rayveleaveme34@inbox.ru>
2022-03-31 22:48:46 +0300

    feat: test switching between all months and between almost all days

--- src/test/java/inc/mimik/fishki_dnet_testing/MainPageTest.java
+++ src/test/java/inc/mimik/fishki_dnet_testing/MainPageTest.java
@@ -7,6 +7,8 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.MessageFormatter;
@@ -98,7 +100,7 @@ public class MainPageTest {
     return $x( "(//div[contains(@class, 'core border monyear title')]//div//span)[2]" );
   }
 
-  private String changeToMonthBegin( ) throws ParseException {
+  private String changeToMonthBegin( int day ) throws ParseException {
     MPAGE.SELECT_DATE_XPATH.shouldBe( exist );
     MPAGE.SELECT_DATE_XPATH.hover( );
 
@@ -116,14 +118,14 @@ public class MainPageTest {
     for ( SelenideElement d : dates ) {
       d.shouldBe( visible );
       // LOGGER.info( "available date: {}", d.text() );
-      if ( d.text().equals( "1" ) ) {
+      if ( d.text().equals( String.valueOf( day ) ) ) {
         begin = d;
         break;
       }
     }
 
     if ( begin == null )
-      throw new NullPointerException( "Can't find the beginning of the month" );
+      throw new NullPointerException( String.format( "Can't find the day [ %d ] of the month", day ) );
 
     LOGGER.info( "Setting date to {} {} {}", begin.text(), month.text(), year.text() );
     final String[] date = new String[]{ begin.text(), month.text(), year.text() };
@@ -137,17 +139,26 @@ public class MainPageTest {
     return beginText;
   }
 
-  @Test
-  public void testDateChangeToMonthBegin( ) throws ParseException {
-    String beginText = changeToMonthBegin();
+  @ParameterizedTest
+  @CsvSource( { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" } )
+  public void testDateChangeToMonthBegin( int day ) throws ParseException {
+    String beginText = changeToMonthBegin( day );
     final SelenideElement FIRST_DAY_SELECT_DATE_XPATH = $x( "//span[contains(@class, 'content__filter-label' )]" );
     FIRST_DAY_SELECT_DATE_XPATH.shouldBe( exist );
     FIRST_DAY_SELECT_DATE_XPATH.shouldHave( text( "за " + beginText ) );
   }
 
-  @Test
-  public void testMonthChangeToFirstJanuary( ) throws ParseException {
-    LOGGER.info( "Set month to January" );
+  private static final String[] MONTHS = {
+      "январь", "февраль", "март",
+      "апрель", "май", "июнь",
+      "июль", "август", "сентябрь",
+      "октябрь", "ноябрь", "декабрь"
+  };
+
+  @ParameterizedTest
+  @CsvSource( { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" } )
+  public void testMonthChangeToFirstOfNthMonthOfPreviousYear( int monthValue ) throws ParseException {
+    LOGGER.info( "Set month to {}", MONTHS[ monthValue ] );
 
     MPAGE.SELECT_DATE_XPATH.shouldBe( exist );
     MPAGE.SELECT_DATE_XPATH.hover( );
@@ -160,20 +171,41 @@ public class MainPageTest {
     nextArrow.shouldBe( visible );
     nextArrow.click();
 
+    // select previous year
+    final SelenideElement year = $x( "(//div[contains(@class, 'core border monyear title')]//div//span)[2]" );
+    year.shouldBe( visible );
+    int nYear = Integer.parseInt( year.text() );
+    nYear = nYear < 2009? 2009 : nYear;
+    LOGGER.info( "Valid year is {}", nYear );
+    year.click( );
+
+    final SelenideElement yearSelector = $x("(//div[contains(@class, 'core border monyear title')]//div//select)[2]" );
+    yearSelector.shouldBe( visible );
+    yearSelector.selectOptionByValue( String.valueOf( nYear - 1 ) );
+
     final SelenideElement month = $x( "(//div[contains(@class, 'core border monyear title')]//div//span)[1]" );
 
     month.shouldBe( visible );
+    final String monthName = month.text();
     month.click();
 
-    final SelenideElement monthSelector = $x( "//div[contains(@class, 'core border monyear title')]//div//select" );
+    final SelenideElement monthSelector = $x( "(//div[contains(@class, 'core border monyear title')]//div//select)[1]" );
+    final boolean isCurrent = MONTHS[ monthValue ].equalsIgnoreCase( monthName );
+    LOGGER.info( "Is testing current month ( {} ): {}", monthName, isCurrent );
+    if ( isCurrent ) {
+      monthSelector.shouldBe( visible );
+      monthSelector.selectOptionByValue( String.valueOf( ( 12 + monthValue - 1 ) % 12 ) ); // to select previous Month
 
-    monthSelector.shouldBe( visible );
-    monthSelector.selectOptionByValue( "0" ); // to select January
+      month.shouldBe( visible );
+      month.click( );
+    }
+
+    monthSelector.selectOptionByValue( String.valueOf( monthValue ) ); // to select Nth Month
 
     month.shouldBe( visible );
-    month.shouldHave( text( "Январь" ) );
+    month.shouldHave( text( MONTHS[ monthValue ] ));
 
-    final String beginText = changeToMonthBegin();
+    final String beginText = changeToMonthBegin( 1 );
 
     final SelenideElement FIRST_DAY_SELECT_DATE_XPATH = $x( "//span[contains(@class, 'content__filter-label' )]" );
     FIRST_DAY_SELECT_DATE_XPATH.shouldBe( exist );
